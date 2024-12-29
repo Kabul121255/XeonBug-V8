3a1906a2d730880a70d8df0051fc65a43ffe50e1
Kabul121255 <ahmadimohammed481@gmail.com>
2024-12-29 17:14:33 +0430

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "93782694353"
+global.ownername = "Ahmadzia Nabizada"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '93782694353'  //creator number
+global.ownername = 'Ahmadzia Nabizada' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
